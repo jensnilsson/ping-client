671d18a4eb7f78cd7ee6904ce50ad51084d9e72e
Jens Nilsson <jens.erik.nilsson@gmail.com>
2020-10-24 23:45:09 +0200

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-//! Hello World client
+//! Ping client
+
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Connecting to hello world server...\n");
+    println!("Connecting to Pong server...\n");
 
     let context = zmq::Context::new();
     let requester = context.socket(zmq::REQ).unwrap();
@@ -10,11 +13,11 @@ fn main() {
 
     let mut msg = zmq::Message::new();
 
-    for request_nbr in 0..10 {
-        println!("Sending Hello {}...", request_nbr);
-        requester.send("Hello", 0).unwrap();
-
+    for request_nbr in 0..51 {
+        println!("Sending Ping {}...", request_nbr);
+        requester.send("Ping", 0).unwrap();
+        thread::sleep(Duration::from_millis(50));
         requester.recv(&mut msg, 0).unwrap();
-        println!("Received World {}: {}", msg.as_str().unwrap(), request_nbr);
+        println!("Received Pong {}: {}", msg.as_str().unwrap(), request_nbr);
     }
 }
